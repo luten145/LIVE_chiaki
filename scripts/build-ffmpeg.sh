601963e3b15dd7cfc887a628b2e4a8b19e4ec55f
luten145 <61258117+luten145@users.noreply.github.com>
2023-07-22 11:01:10 +0900

    Update build-ffmpeg.sh

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -7,6 +7,10 @@ ROOT="`pwd`"
 
 TAG=n4.3.1
 
+export PATH="/c/msys64/mingw64/bin:${PATH}"
+export PATH="/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/Hostx64/x64:$PATH"
+export CC="/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/Hostx64/x64/cl.exe"
+
 git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || exit 1
 
 ./configure --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vaapi --enable-hwaccel=hevc_vaapi --prefix="$ROOT/ffmpeg-prefix" "$@" || exit 1
