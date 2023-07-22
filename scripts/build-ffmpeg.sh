eec7155a18a7b285101e9ce8ff74171ef74f63c3
luten145 <61258117+luten145@users.noreply.github.com>
2023-07-22 18:03:03 +0900

    aww

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -7,8 +7,22 @@ ROOT="`pwd`"
 
 TAG=n4.3.1
 
-git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || exit 1
 
-./configure --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vaapi --enable-hwaccel=hevc_vaapi --prefix="$ROOT/ffmpeg-prefix" "$@" || exit 1
+
+DIR="/ffmpeg"
+
+if [ -d "$DIR" ]; then
+  echo "$DIR exists."
+  # 디렉터리가 존재하면 실행할 명령어를 여기에 작성하세요.
+make install || exit 1
+else
+  echo "$DIR does not exist."
+  # 디렉터리가 존재하지 않으면 실행할 명령어를 여기에 작성하세요.
+  git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || exit 1
+
+  ./configure --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vaapi --enable-hwaccel=hevc_vaapi --prefix="$ROOT/ffmpeg-prefix" "$@" || exit 1
 make -j4 || exit 1
 make install || exit 1
+
+fi
+
