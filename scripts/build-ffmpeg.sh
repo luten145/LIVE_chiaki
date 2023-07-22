b1c304d8701ab4be658c87d7925a7c9d7be96837
luten145 <61258117+luten145@users.noreply.github.com>
2023-07-22 18:07:48 +0900

    eewe

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -14,8 +14,9 @@ DIR="ffmpeg"
 if [ -d "$DIR" ]; then
   echo "$DIR exists."
   # 디렉터리가 존재하면 실행할 명령어를 여기에 작성하세요.
-    ./configure --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vaapi --enable-hwaccel=hevc_vaapi --prefix="$ROOT/ffmpeg-prefix" "$@" || exit 1
-make -j4 || exit 1
+
+  cd ffmpeg
+    
 make install || exit 1
 else
   echo "$DIR does not exist."
